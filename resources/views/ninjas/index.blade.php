afeff5d43497b20b78215fbbd5fffe942ea14d62
flauschio <danendramdn@gmail.com>
2025-09-03 20:26:15 +0700

    initial commit

---
+++ resources/views/ninjas/index.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja Netwok | Home</title>
+</head>
+<body>
+    <h2> Currently Available Ninjas</h2>
+    
+    <ul>
+        <li>Ninjas Here</li>
+
+</body>
+</html>
